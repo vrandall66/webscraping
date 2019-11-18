6c06eb29eb38ccecdd79203277ea947f88211d21
Vanessa Randall <vanessalrandall@gmail.com>
2019-11-17 17:11:26 -0700

    Create Pokemon array with ids and names of randomized Pokemon

--- scrape.js
+++ scrape.js
@@ -8,12 +8,27 @@ nightmare
   .wait(750)
   .click('a[id="shuffle"]')
   .wait(600)
-  .evaluate(function () {
-    var nameNodes = document.querySelectorAll('h5');
-    var list = Array.from(nameNodes);
-    return list.map(function (node) {
-      return node.innerText
+  .evaluate(function() {
+    let nameNodes = document.querySelectorAll('h5');
+    let idNodes = document.querySelectorAll('p.id');
+    let typeNodes = document.querySelectorAll('div.abilities')
+    let nameList = Array.from(nameNodes);
+    let idList = Array.from(idNodes);
+    let names = nameList.map(function(node) {
+      return node.innerText;
     });
+    let ids = idList.map(function(node) {
+      return node.innerText;
+    });
+
+    let pokemon = names.reduce((acc, current, index) => {
+      let id = ids[index]
+      let newPokemon = { Name: current, ID: id }
+      !acc.includes(newPokemon) ? acc.push(newPokemon) : null;
+      return acc;
+    }, [])
+
+    return pokemon;
   })
   .end()
   .then(result => {
